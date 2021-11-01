ebadb39df60e8c04515fe2acb648183d8bc691d5
丸尾亮太 <maruoryouta@Ryota-Maruo.local>
2021-11-01 09:43:04 +0900

    commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/download', views.download, name='download')
+    path('download', views.download, name='download')
 ]
